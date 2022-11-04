7f4a1be98da34b90921db21013a0d4fdd199ae15
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-11-03 21:14:16 -0400

    created first set of questions

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 import inquirer from "inquirer";
+import Question from "./lib/question.js";
+import Questions from "./lib/questions.js"
+import Answer from "./lib/answer.js";
+
+function init() {
+    let questions = [
+        new Question("What is the team manager's name?", "manager", "", ""),
+        new Question("What is the employee ID for the manager?", "id", "", ""),
+        new Question("What is the email address for the manager?", "email", "", ""),
+        new Question("What is the office number?", "officeNumber", "", ""),
+    ];
+
+    return new Questions(questions);
+}
 
 function main() {
+    let questions = init();
     const answers = inquirer.prompt(questions);
 }
 
