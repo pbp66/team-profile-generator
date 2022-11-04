602310284efa7966f6022e9c5cde2fce49c88f5e
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-11-03 22:36:50 -0400

    Basic inquirer loop to generate as many employees as desired

--- index.js
+++ index.js
@@ -5,19 +5,60 @@ import Answer from "./lib/answer.js";
 
 function init() {
     const questions = [
-        new Question("What is the team manager's name?", "manager", "", ""),
-        new Question("What is the employee ID for the manager?", "id", "", ""),
-        new Question("What is the email address for the manager?", "email", "", ""),
-        new Question("What is the office number?", "officeNumber", "", ""),
+        new Question("What is the manager's name?", "manager", "", ""),
+        new Question("What is the manager's employee ID?", "id", "", ""),
+        new Question("What is the manager's email address?", "email", "", ""),
+        new Question("What is the manager's office number?", "officeNumber", "", ""),
     ];
 
     return new Questions(questions);
 }
 
 async function main() {
-    let questions = init();
-    const answers = await questions.askQuestions();
+    const managerQuestions = init();
+    const engineerQuestionList = [
+        new Question("What is the engineer's name?", "engineer", "", ""),
+        new Question("What is the engineer's employee ID?", "id", "", ""),
+        new Question("What is the engineer's email address?", "email", "", ""),
+        new Question("What is the engineer's Github username?", "github", "", "")
+    ];
+    const internQuestionList = [
+        new Question("What is the intern's name?", "engineer", "", ""),
+        new Question("What is the intern's employee ID?", "id", "", ""),
+        new Question("What is the intern's email address?", "email", "", ""),
+        new Question("What is the intern's school?", "school", "", "")
+    ];
+
+    const engineerQuestions = new Questions(engineerQuestionList);
+    const internQuestions = new Questions(internQuestionList);
+    const choices = ["Engineer", "Intern", "Finish Building Team"];
+    const teamBuilding = new Question("Do you want to add another employee?", "addEmployees", "rawlist", 0, choices);
+
+    let teamResponse = {};
+    let response = {};
+    let endQuestions = false;
+
+    let answers = await managerQuestions.askQuestions();
+    console.log("\n");
+
     console.log(answers);
+    console.log("\n");
+    do {
+        teamResponse = await inquirer.prompt([ teamBuilding ]);
+        if (teamResponse.addEmployees === "Engineer") {
+            response = await engineerQuestions.askQuestions();
+            console.log(response);
+            console.log("\n");
+        } else if (teamResponse.addEmployees === "Intern") {
+            response = await internQuestions.askQuestions();
+            console.log(response);
+            console.log("\n");
+        } else {
+            // Complete team building
+            endQuestions = true;
+        }
+    } while (!endQuestions)
+
 }
 
 main();
