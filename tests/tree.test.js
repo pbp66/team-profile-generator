ec251acea5bd280057570586c09832cba5912b43
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-11-07 21:12:28 -0500

    Added some test descriptions to tree.test.js

--- tests/tree.test.js
+++ tests/tree.test.js
@@ -1,5 +1,51 @@
-import Node from "../lib/node.js";
+import Tree from "../lib/tree.js";
 
-describe("Node", () => {
-    
-})
+describe("Tree", () => {
+    it("Returns a Tree object with root, depth, and size", () => {
+
+    });
+
+    it("Returns a Tree object with appropriate root, depth, and size values when instantiated.", () => {
+
+    });
+
+    describe("root()", () => {
+        it("Returns the root node of the tree object", () => {
+
+        });
+    });
+
+    describe("add(node)", () => {
+
+    });
+
+    describe("remove(node)", () => {
+
+    });
+
+    describe("contains(node)", () => {
+
+    });
+
+    describe("size()", () => {
+        it("Returns the total number of nodes in the tree", () => {
+
+        });
+    });
+
+    describe("height()", () => {
+        it("Returns the total height of the tree", () => {
+
+        });
+    });
+
+    describe("isEmpty()", () => {
+        it("Returns true if the tree contains zero nodes", () => {
+
+        });
+
+        it("Returns false if the tree contains at least one node", () => {
+
+        });
+    });
+});
