6072bf6d90016d0b27fff5286930a815b6633c1c
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-11-07 19:36:34 -0500

    Completed all tests for employee class

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -9,9 +9,35 @@ describe("Employee", () => {
         expect("email" in employee).toEqual(true);
     });
 
+    it("Returns an object with appropriate name, id, and email values when instantiated", ()=> {
+        const employee = new Employee("John Test", 1234, "john.test@test.com");
+        
+        expect(employee.name === "John Test").toEqual(true);
+        expect(employee.id === 1234).toEqual(true);
+        expect(employee.email === "john.test@test.com").toEqual(true);
+    });
+
     it("Returns name when the getName() method is called", () => {
         const employee = new Employee("John Test", 1234, "john.test@test.com");
 
-        
-    })
+        expect(employee.getName() === "John Test").toEqual(true);
+    });
+
+    it("Returns name when the getId() method is called", () => {
+        const employee = new Employee("John Test", 1234, "john.test@test.com");
+
+        expect(employee.getId() === 1234).toEqual(true);
+    });
+
+    it("Returns name when the getEmail() method is called", () => {
+        const employee = new Employee("John Test", 1234, "john.test@test.com");
+
+        expect(employee.getEmail() === "john.test@test.com").toEqual(true);
+    });
+
+    it("Returns name when the getRole() method is called", () => {
+        const employee = new Employee("John Test", 1234, "john.test@test.com");
+
+        expect(employee.getRole() === "Employee").toEqual(true);
+    });
 });
